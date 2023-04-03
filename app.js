cbdb10ca876b6059488eb96eba73d66f469de0de
antarah <howalantara@gmail.com>
2023-04-04 02:28:35 +0530

    changes

--- app.js
+++ app.js
@@ -22,14 +22,6 @@ app.use(cors());
 app.use(cookieParser(""));
 
 app.use(router);
-
-// Allow all domains
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next()
-  })
  //app.get("/",(req,res)=>{
  //    res.send("your server is running");
 // });
