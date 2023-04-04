195857a422ed9f977797c232a3f353c6930e286b
antarah <howalantara@gmail.com>
2023-04-04 14:52:53 +0530

    changes

--- app.js
+++ app.js
@@ -18,7 +18,20 @@ const port = process.env.PORT || 8005;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+		origin: function (origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	}
+));
 app.use(cookieParser(""));
 
 app.use(router);
